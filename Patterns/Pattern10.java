a4cd371d71ac1f4cf9120590f0ada6bb880a5247
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-13 01:50:59 +0530

    Pattern 10

---
+++ Patterns/Pattern10.java
@@ -0,0 +1,36 @@
+package DSA_Nados.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern10 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int row=1,nst=1,nsp=n/2;
+        while(row<=n){
+            //print spaces
+            for(int i=0;i<nsp;i++){
+                System.out.print("\t");
+            }
+            //print stars
+            for(int i=0;i<nst;i++){
+                if(i==0 || i==nst-1)
+                System.out.print("*\t");
+                else
+                System.out.print("\t");
+            }
+            //prep
+            if(row<=n/2){
+                nsp--;
+                nst+=2;
+            }
+            else{
+                nsp++;
+                nst-=2;
+            }
+            row++;
+            System.out.println();
+        }
+        scn.close();
+     }
+}
