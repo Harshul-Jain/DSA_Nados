7865a141ec9fa114d7015cab58eb139871f2e873
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-12 23:23:41 +0530

    Count Digits In Number

---
+++ CountDigits.java
@@ -0,0 +1,18 @@
+package DSA_Nados;
+
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        // write your code here  
+        Scanner scn=new Scanner(System.in);
+        int count=0;
+        int n=scn.nextInt();
+        while(n>0){
+          n=n/10;
+          count++;
+        }
+        System.out.println(count);
+        scn.close();
+    }
+}
