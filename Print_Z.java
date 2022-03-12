6c3a63a7ddb4d07e0b9fd150ad886ab9da90a483
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-12 22:53:50 +0530

    Print_Z

---
+++ Print_Z.java
@@ -0,0 +1,15 @@
+package DSA_Nados;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+
+    }
+}
