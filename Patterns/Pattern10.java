3d2a8ad367c24331261732776a9d0086eea49e96
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-13 02:10:20 +0530

    Pattern 10 Approach 2

--- Patterns/Pattern10.java
+++ Patterns/Pattern10.java
@@ -6,27 +6,55 @@ public class Pattern10 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n=scn.nextInt();
-        int row=1,nst=1,nsp=n/2;
+        // int row=1,nst=1,nsp=n/2;
+        // while(row<=n){
+        //     //print spaces
+        //     for(int i=0;i<nsp;i++){
+        //         System.out.print("\t");
+        //     }
+        //     //print stars
+        //     for(int i=0;i<nst;i++){
+        //         if(i==0 || i==nst-1)
+        //         System.out.print("*\t");
+        //         else
+        //         System.out.print("\t");
+        //     }
+        //     //prep
+        //     if(row<=n/2){
+        //         nsp--;
+        //         nst+=2;
+        //     }
+        //     else{
+        //         nsp++;
+        //         nst-=2;
+        //     }
+        //     row++;
+        //     System.out.println();
+        // }
+        /* Approach-2 */
+        int row=1,nsp=n/2,nisp=-1;
         while(row<=n){
-            //print spaces
+            //Print spaces
             for(int i=0;i<nsp;i++){
                 System.out.print("\t");
             }
-            //print stars
-            for(int i=0;i<nst;i++){
-                if(i==0 || i==nst-1)
-                System.out.print("*\t");
-                else
+            //Print star
+            System.out.print("*\t");
+            //print inner space
+            for(int i=0;i<nisp;i++){
                 System.out.print("\t");
             }
+            //Print star
+            if(row>1 & row<n)
+            System.out.print("*\t");
             //prep
             if(row<=n/2){
                 nsp--;
-                nst+=2;
+                nisp+=2;
             }
             else{
                 nsp++;
-                nst-=2;
+                nisp-=2;
             }
             row++;
             System.out.println();
