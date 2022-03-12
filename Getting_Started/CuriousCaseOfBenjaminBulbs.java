0853a8a32da302647d26465fa6467a437c4d939e
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-12 23:43:30 +0530

    Benjamin Bulbs

---
+++ Getting_Started/CuriousCaseOfBenjaminBulbs.java
@@ -0,0 +1,15 @@
+package DSA_Nados.Getting_Started;
+
+import java.util.Scanner;
+
+public class CuriousCaseOfBenjaminBulbs {
+    public static void main(String[] args) {
+        // write your code here  
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        for(int i=1;i*i<=n;i++){
+          System.out.println(i*i);
+        }
+        scn.close();
+       }
+}
