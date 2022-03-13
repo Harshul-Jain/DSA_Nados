cc956c68261c537442c763000645ab89a69abc80
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-13 20:27:08 +0530

    Pattern 19 Approach 2

---
+++ Patterns/Pattern19_2.java
@@ -0,0 +1,52 @@
+package DSA_Nados.Patterns;
+import java.util.*;
+
+public class Pattern19_2 {
+
+    public static void main(String[] args){
+        Scanner scn =new Scanner(System.in);
+        int n= scn.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1){
+                    if(j==n || j<=n/2 +1){
+                        System.out.print("*\t");
+                    }
+                    else{
+                        System.out.print("\t");
+                    }
+                }
+                else if(i<=n/2){
+                    if(j==n || j==n/2 + 1){
+                        System.out.print("*\t");
+                    }
+                    else{
+                        System.out.print("\t");
+                    }
+                }
+                else if(i==n/2 + 1){
+                    System.out.print("*\t");
+                }
+                else if(i<n){
+                    if(j==1 || j== n/2 +1){
+                        System.out.print("*\t");
+                    }
+                    else{
+                        System.out.print("\t");
+                    }
+                }
+                else{
+                    if(j==1 || j>= n/2 + 1){
+                        System.out.print("*\t");
+                    }
+                    else{
+                        System.out.print("\t");
+                    }
+                }
+
+            }
+            System.out.println();
+        }
+        scn.close();
+    }
+}
