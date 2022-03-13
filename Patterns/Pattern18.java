0f1dafd55fe2b651510883b8248a9447673cf659
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-13 16:19:13 +0530

    Pattern 18

---
+++ Patterns/Pattern18.java
@@ -0,0 +1,40 @@
+package DSA_Nados.Patterns;
+
+import java.util.Scanner;
+
+public class Pattern18 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int row=1,nsp=0,nst=n;
+        while(row<=n){
+            //Print space
+            for(int i=0;i<nsp;i++){
+                System.out.print("\t");
+            }
+            //Print stars
+            for(int i=0;i<nst;i++){
+                if(row>1 && row<=n/2){
+                    if(i==0 || i==nst-1)
+                        System.out.print("*\t");
+                    else
+                        System.out.print("\t");
+                }
+                else
+                    System.out.print("*\t");
+            }
+            //prep
+            if(row<=n/2){
+                nsp++;
+                nst-=2;
+            }
+            else{
+                nsp--;
+                nst+=2;
+            }
+            row++;
+            System.out.println();
+        }
+        scn.close();
+     }
+}
