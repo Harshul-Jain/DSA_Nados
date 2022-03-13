d5a626087e3105afa5648d39bba966212dedab56
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-13 20:42:02 +0530

    Digit Frequency

---
+++ Function_And_Arrays/DigitFreuency.java
@@ -0,0 +1,25 @@
+package DSA_Nados.Function_And_Arrays;
+
+import java.util.Scanner;
+
+public class DigitFreuency {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int d = scn.nextInt();
+        int f = getDigitFrequency(n, d);
+        System.out.println(f);
+        scn.close();
+    }
+
+    public static int getDigitFrequency(int n, int d) {
+        int count=0;
+        while(n!=0){
+            int digit = n%10;
+            if(digit==d)
+                count++;
+            n=n/10;
+        }
+        return count;
+    }
+}
