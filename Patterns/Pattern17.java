027c8f123ad0f35528b2ef13e12452590f530f3f
Harshul Jain <harshuljain.mait.it@gmail.com>
2022-03-13 15:42:17 +0530

    Pattern 17

---
+++ Patterns/Pattern17.java
@@ -0,0 +1,35 @@
+package DSA_Nados.Patterns;
+
+public class Pattern17 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int row=1,nsp=n/2,nst=1;
+        while(row<=n){
+            //Print space
+            for(int i=0;i<nsp;i++){
+                System.out.print("\t");
+            }
+            //Print stars
+            for(int i=0;i<nst;i++){
+                if( i>=nst/2 || row==(n/2)+1)
+                    System.out.print("*\t");
+                else
+                    System.out.print("\t");
+            }
+            //prep
+            if(row<=n/2){
+                nsp--;
+                nst+=2;
+            }
+            else{
+                nst-=2;
+                nsp++;
+            }
+            row++;
+            System.out.println();
+
+        }
+
+    }
+}
